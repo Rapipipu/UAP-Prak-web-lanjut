19939c4f17be44185bb15e3115bf0cdb7a78cd2d
Muhammad Rafi Satria <mrafisatria@gmail.com>
2023-11-23 22:17:36 +0700

    benerin auth kedua kalinya

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -93,10 +93,7 @@ class AuthController extends Controller
             return redirect()->to(route_to('reset-password') . '?token=' . $this->auth->user()->reset_hash)->withCookies();
         }
 
-        $redirectURL = session('redirect_url') ?? base_url('/auth');
-        unset($_SESSION['redirect_url']);
-
-        return redirect()->to($redirectURL)->withCookies()->with('message', lang('Auth.loginSuccess'));
+        return redirect()->to(base_url('/auth'))->withCookies()->with('message', lang('Auth.loginSuccess'));
     }
 
     /**
